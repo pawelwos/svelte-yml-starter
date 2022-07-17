5dd9ada246a6cc5c86249b7a86a4caf697061cf1
Pawel Wos <pawelwos@gmail.com>
2022-07-17 09:34:16 +0200

    Added TailwindCSS

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import preprocess from "svelte-preprocess";
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+		prerender: {
+			default: true
+		}
+	},
 };
 
 export default config;
