7e445bdca8d51ce417a28abda3c49db5fc683c9a
Pawel Wos <pawelwos@gmail.com>
2022-07-27 18:06:39 +0200

    Removed adapter-static and installed adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from "svelte-preprocess";
 
 
@@ -10,13 +10,20 @@ const config = {
     }),
   ],
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		}),
+    adapter: vercel({
+      // if true, will deploy the app using edge functions
+      // (https://vercel.com/docs/concepts/functions/edge-functions)
+      // rather than serverless functions
+      edge: false,
+
+      // an array of dependencies that esbuild should treat
+      // as external when bundling functions
+      external: [],
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app
+      split: false
+    }),
 		prerender: {
 			default: true
 		}
